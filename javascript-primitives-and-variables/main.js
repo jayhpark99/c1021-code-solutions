3ddef64423105e3bd2a2c9a4b23e2ab7bdbde952
Jay Park <jayhpark99@gmail.com>
2021-10-11 12:50:32 -0700

    completed primitive and variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,15 @@
+var fullName = 'Jay Park';
+console.log('value of fullName:', fullName);
+var isCool = true;
+console.log('value of isCool:', isCool);
+var totalPets = 0;
+console.log('value of totalPets:', totalPets);
+console.log('type of fullName:', typeof (fullName));
+console.log('type of isCool:', typeof (isCool));
+console.log('type of totalPets:', typeof (totalPets));
+var unknown = null;
+console.log('value of unknown:', unknown);
+console.log('type of unknown:', typeof (unknown));
+var never;
+console.log('value of never:', never);
+console.log('type of never:', typeof (never));
