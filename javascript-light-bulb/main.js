01fa71c56b4b2ee16a3f02ff6dffbec8f907c940
Jay Park <jayhpark99@gmail.com>
2021-10-19 11:41:16 -0700

    changes made

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,13 +1,13 @@
-var count = 0;
+var swap = true;
 var $lightbulb = document.querySelector('.lightbulb');
 var $background = document.querySelector('.background');
 $lightbulb.addEventListener('click', increment);
 function increment() {
-  count++;
-  if (count % 2 === 0) {
+  swap = !swap;
+  if (swap === true) {
     $lightbulb.className = 'lightbulb on';
     $background.className = 'background backgroundon';
-  } if (count % 2 !== 0) {
+  } if (swap === false) {
     $lightbulb.className = 'lightbulb off';
     $background.className = 'background backgroundoff';
   }
