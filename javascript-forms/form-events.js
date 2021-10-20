d9e914ae8bdd6c0a8f0462de59ecb564ae9f99c6
Jay Park <jayhpark99@gmail.com>
2021-10-20 11:45:42 -0700

    changes made2

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -12,16 +12,17 @@ function handleInput(event) {
   console.log('value of', event.target.name, event.target.value);
 }
 
-var $input = document.querySelectorAll('input');
+var $input1 = document.querySelector('#user-name');
+var $input2 = document.querySelector('#user-email');
 var $textarea = document.querySelector('textarea');
 
-$input[0].addEventListener('focus', handleFocus);
-$input[0].addEventListener('blur', handleBlur);
-$input[0].addEventListener('input', handleInput);
+$input1.addEventListener('focus', handleFocus);
+$input1.addEventListener('blur', handleBlur);
+$input1.addEventListener('input', handleInput);
 
-$input[1].addEventListener('focus', handleFocus);
-$input[1].addEventListener('blur', handleBlur);
-$input[1].addEventListener('input', handleInput);
+$input2.addEventListener('focus', handleFocus);
+$input2.addEventListener('blur', handleBlur);
+$input2.addEventListener('input', handleInput);
 
 $textarea.addEventListener('focus', handleFocus);
 $textarea.addEventListener('blur', handleBlur);
