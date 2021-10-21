c122504c90d9532c45f4994b77e104deb753ca51
Jay Park <90732703+jayhpark99@users.noreply.github.com>
2021-10-21 15:36:08 -0700

    Update main.js

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,5 +1,5 @@
 var $taskList = document.querySelector('.task-list');
-$taskList.addEventListener('click', function () {
+$taskList.addEventListener('click', function (event) {
   console.log('event.target', event.target);
   console.log('event.target.tagName', event.target.tagName);
   if (event.target.tagName === 'BUTTON') {
