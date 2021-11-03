b43b3a19126ca0b92130bec7e0a554b1b893c07f
Jay Park <jayhpark99@gmail.com>
2021-11-03 16:40:45 -0700

    huge changes

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,99 +1,54 @@
 var $right = document.querySelector('.fa-angle-right');
 var $left = document.querySelector('.fa-angle-left');
-var $bulbasoarButton = document.querySelector('.bulbasoar');
-var $charmanderButton = document.querySelector('.charmander');
-var $squirtleButton = document.querySelector('.squirtle');
-var $pikachuButton = document.querySelector('.pikachu');
-var $jigglypuffButton = document.querySelector('.jigglypuff');
 var $img = document.querySelector('img');
 var $outerdiv = document.querySelector('.outerdiv');
+var $buttons = document.querySelectorAll('button');
 
-var view = 'bulbasoar';
+var currentImg = 0;
+var imgArray = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
 
-$outerdiv.addEventListener('click', function (event) {
+$outerdiv.addEventListener('click', changeImage);
+function changeImage(event) {
   if (event.target.tagName === 'BUTTON') {
-    if (event.target.className === 'bulbasoar') {
-      $img.setAttribute('src', 'images/001.png');
-      view = 'bulbasoar';
-    } else if (event.target.className === 'charmander') {
-      $img.setAttribute('src', 'images/004.png');
-      view = 'charmander';
-    } else if (event.target.className === 'squirtle') {
-      $img.setAttribute('src', 'images/007.png');
-      view = 'squirtle';
-    } else if (event.target.className === 'pikachu') {
-      $img.setAttribute('src', 'images/025.png');
-      view = 'pikachu';
-    } else if (event.target.className === 'jigglypuff') {
-      $img.setAttribute('src', 'images/039.png');
-      view = 'jigglypuff';
-    }
-    changeButton();
+    currentImg = parseInt(event.target.getAttribute('data'));
+    $img.setAttribute('src', imgArray[currentImg]);
+    change();
   }
-});
+}
 
 $right.addEventListener('click', nextImage);
 
 function nextImage() {
-  if (view === 'bulbasoar') {
-    $img.setAttribute('src', 'images/004.png');
-    view = 'charmander';
-  } else if (view === 'charmander') {
-    $img.setAttribute('src', 'images/007.png');
-    view = 'squirtle';
-  } else if (view === 'squirtle') {
-    $img.setAttribute('src', 'images/025.png');
-    view = 'pikachu';
-  } else if (view === 'pikachu') {
-    $img.setAttribute('src', 'images/039.png');
-    view = 'jigglypuff';
-  } else if (view === 'jigglypuff') {
-    $img.setAttribute('src', 'images/001.png');
-    view = 'bulbasoar';
+  currentImg++;
+  if (currentImg >= imgArray.length) {
+    currentImg = 0;
   }
-  changeButton();
+  change();
 }
 
 $left.addEventListener('click', previousImage);
 function previousImage(event) {
-  if (view === 'bulbasoar') {
-    $img.setAttribute('src', 'images/039.png');
-    view = 'jigglypuff';
-  } else if (view === 'charmander') {
-    $img.setAttribute('src', 'images/001.png');
-    view = 'bulbasoar';
-  } else if (view === 'squirtle') {
-    $img.setAttribute('src', 'images/004.png');
-    view = 'charmander';
-  } else if (view === 'pikachu') {
-    $img.setAttribute('src', 'images/007.png');
-    view = 'squirtle';
-  } else if (view === 'jigglypuff') {
-    $img.setAttribute('src', 'images/025.png');
-    view = 'pikachu';
+  currentImg--;
+  if (currentImg < 0) {
+    currentImg = imgArray.length - 1;
   }
-  changeButton();
+  change();
 }
 
 function changeButton() {
-  $bulbasoarButton.style.backgroundColor = 'white';
-  $charmanderButton.style.backgroundColor = 'white';
-  $squirtleButton.style.backgroundColor = 'white';
-  $pikachuButton.style.backgroundColor = 'white';
-  $jigglypuffButton.style.backgroundColor = 'white';
-  if (view === 'bulbasoar') {
-    $bulbasoarButton.style.backgroundColor = 'black';
-  } else if (view === 'charmander') {
-    $charmanderButton.style.backgroundColor = 'black';
-  } else if (view === 'squirtle') {
-    $squirtleButton.style.backgroundColor = 'black';
-  } else if (view === 'pikachu') {
-    $pikachuButton.style.backgroundColor = 'black';
-  } else if (view === 'jigglypuff') {
-    $jigglypuffButton.style.backgroundColor = 'black';
+  for (var i = 0; i < $buttons.length; i++) {
+    $buttons[i].style.backgroundColor = 'white';
+    if (currentImg === i) {
+      $buttons[i].style.backgroundColor = 'black';
+    }
   }
   clearInterval(interval);
   interval = setInterval(nextImage, 3000);
 }
 
 var interval = setInterval(nextImage, 3000);
+
+function change() {
+  $img.setAttribute('src', imgArray[currentImg]);
+  changeButton();
+}
