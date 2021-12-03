6fac08804f2558a82779e66447b79856c702ebd2
Jay Park <jayhpark99@gmail.com>
2021-12-03 23:05:24 +0000

    movie title instead of id

--- sql-join/magic-mallrats.sql
+++ sql-join/magic-mallrats.sql
@@ -2,5 +2,8 @@ select "c"."firstName", "c"."lastName"
   from "customers"
   join "rentals" using ("customerId")
   join "inventory" using ("inventoryId")
+  join "films" as "f" using ("filmId")
   join "customers" as "c" using ("customerId")
- where "inventoryId" = 2494 or "inventoryId" = 2495
+ where "f"."title" = 'Magic Mallrats'
+
+--  inventoryId" = 2494 or "inventoryId" = 2495
