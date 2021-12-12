71ce0549513241a49ac18fa617bab57d00abce0e
Jay Park <jayhpark99@gmail.com>
2021-12-12 04:11:33 +0000

    completed react-stopwatch-componenent

--- react-stopwatch-component/src/index.jsx
+++ react-stopwatch-component/src/index.jsx
@@ -6,52 +6,46 @@ class Stopwatch extends React.Component {
     super(props)
     this.state = {
       isPaused: true,
-      secondsPassed: 0
+      secondsPassed: 3
     }
     this.handleIconClick = this.handleIconClick.bind(this)
     this.handleWatchClick = this.handleWatchClick.bind(this)
   }
   handleIconClick() {
-    if (event.target.className === 'fas fa-play') {
+    if (this.state.isPaused === true) {
       this.timerID = setInterval(
         () => this.setState({
           isPaused: false,
           secondsPassed: this.state.secondsPassed + 1 }),
         1000
       )
-    } else if (event.target.className === 'fas fa-pause') {
+    } else if (this.state.isPaused === false) {
       this.setState({isPaused: true})
       clearInterval(this.timerID)
     }
   }
-
   handleWatchClick() {
     if (this.state.isPaused === true) {
       this.setState({ secondsPassed: 0 })
     }
   }
-
   render() {
+    let className
     if (this.state.isPaused === true) {
-      return (
-        <div>
-          <div onClick={this.handleWatchClick} className="circle d-flex justify-content-center align-items-center">
-            <h3>{this.state.secondsPassed}</h3>
-          </div>
-          <i onClick={this.handleIconClick} className="fas fa-play"></i>
-        </div>
-      )
+      className = 'fas fa-play'
     } else {
-      return (
-        <div>
-          <div onClick={this.handleWatchClick} className="circle d-flex justify-content-center align-items-center">
-            <h3>{this.state.secondsPassed}</h3>
-          </div>
-          <i onClick={this.handleIconClick} className="fas fa-pause"></i>
+      className = 'fas fa-pause'
+    }
+    return (
+      <div className="outer">
+        <div onClick={this.handleWatchClick} className="circle">
+          <h3>{this.state.secondsPassed}</h3>
         </div>
+        <i onClick={this.handleIconClick} className={className}></i>
+      </div>
       )
     }
   }
-}
+
 
 ReactDOM.render(<Stopwatch/>, document.querySelector('#root'))
