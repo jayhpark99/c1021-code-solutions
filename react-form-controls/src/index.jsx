3aa3910da952501615be8ff34ed96930a415ef64
Jay Park <jayhpark99@gmail.com>
2021-12-13 20:51:31 +0000

    completed react-form-controls

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {email: ''}
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange(e) {
+    this.setState({email: e.target.value})
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log('state:', this.state)
+  }
+  render() {
+    return (
+    <form onSubmit={this.handleSubmit}>
+      <label>
+        Email:
+        <input onChange={this.handleChange} type="email" name="email"/>
+      </label>
+      <input type="submit" value="Sign Up"/>
+    </form>
+    )
+  }
+}
+
+ReactDOM.render(<NewsletterForm/>, document.querySelector('#root'))
