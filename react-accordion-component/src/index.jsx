07809d89b528d34a006e9057e78b777da666b749
Jay Park <jayhpark99@gmail.com>
2021-12-14 09:00:35 +0000

    project set up

---
+++ react-accordion-component/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Accordion from './accordion'
+
+const element = <Accordion />
+
+ReactDOM.render(element, document.querySelector('#root'))
