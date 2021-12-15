8119f2e05b9fedc709e1a6d7fb94a3f6267323ef
Jay Park <jayhpark99@gmail.com>
2021-12-15 20:40:26 +0000

    changes

--- fetch/main.js
+++ fetch/main.js
@@ -3,9 +3,11 @@ fetch('https://jsonplaceholder.typicode.com/users', {
 })
 .then(response => response.json())
 .then(data => console.log(data))
+.catch(error => console.error(error))
 
 fetch('https://pokeapi.co/api/v2/pokemon/150', {
   method: 'GET'
 })
 .then(response => response.json())
 .then(data => console.log(data))
+.catch(error => console.error(error))
